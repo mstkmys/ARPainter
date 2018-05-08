77558fbf8b61cef544b89803ad934593cf2b1e24
Masataka Miyoshi <masataka.ehime.univ@gmail.com>
2018-05-09 08:42:22 +0900

    Initial Commit

---
+++ ARPainter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ARPainter
+//
+//  Created by Miyoshi Masataka on 2018/05/09.
+//  Copyright © 2018年 Masataka Miyoshi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
