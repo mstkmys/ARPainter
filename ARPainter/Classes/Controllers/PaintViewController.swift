0a613ca055f52bbce427e7d7565d39328f8a0c94
Masataka Miyoshi <masataka.ehime.univ@gmail.com>
2018-05-09 09:15:52 +0900

    ARSCNViewの設定を追加して、run

--- ARPainter/Classes/Controllers/PaintViewController.swift
+++ ARPainter/Classes/Controllers/PaintViewController.swift
@@ -16,16 +16,37 @@ class PaintViewController: UIViewController {
     @IBOutlet weak var scnView: ARSCNView!
     @IBOutlet weak var paintButton: UIButton!
     
+    internal var brushSettings: BrushSettings!
+    private let configuration = ARWorldTrackingConfiguration()
+    
     // MARK: - Life Cycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let rootTabBarController = self.tabBarController as! RootTabBaViewController
+        brushSettings = rootTabBarController.burushSettings
+        
+        // delegate
+        scnView.delegate = self
+        
+        scnView.debugOptions = [ARSCNDebugOptions.showFeaturePoints]
+        scnView.showsStatistics = false
+        scnView.autoenablesDefaultLighting = true
+        
+        scnView.session.run(configuration)
     }
     
     // MARK: - Actions
     
     @IBAction func paintButtonTapped(_ sender: UIButton) {
     }
+    
+}
+
+// MARK: - ARSCNViewDelgate
+/*******************************************************************************************/
+extension PaintViewController: ARSCNViewDelegate {
     
 }
 
